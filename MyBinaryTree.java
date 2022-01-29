df8e0ec15033ebf08e5665aaf2db57562595d13c
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-29 14:26:03 +0530

    Added: Initial Commit

---
+++ MyBinaryTree.java
@@ -0,0 +1,41 @@
+package com.BinarySearchTree;
+
+public class MyBinaryTree<K extends Comparable<K>> {
+	  private MyBinaryNode<K> root;
+
+	    public void add(K key){
+	        this.root = this.addRecursively(root,key);
+	    }
+
+	    private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
+	        if(current == null)
+	            return new MyBinaryNode<>(key);
+	        int compareResult = key.compareTo(current.key);
+	        if(compareResult == 0)
+	            return current;
+	        if (compareResult < 0){
+	            current.left = addRecursively(current.left, key);
+
+	        }else{
+	            current.right = addRecursively(current.right, key);
+	        }
+	        return current;
+	    }
+	    public int getSize(){
+	        return this.getsizeRecursively(root);
+	    }
+	    private int getsizeRecursively(MyBinaryNode<K> current) {
+	    	return current == null ? 0 : 1 + this.getsizeRecursively(current.left)
+	    	                               + this.getsizeRecursively(current.left);
+	    }
+	    public void print() {
+	        print(root);
+	    }
+	    void print(MyBinaryNode root) {
+	        if(root != null) {
+	            print(root.left);
+	            System.out.println(root.key);
+	            print(root.right);
+	        }
+	    }
+}
---
+++ MyBinaryTreeMain.java
@@ -0,0 +1,17 @@
+package com.BinarySearchTree;
+
+public class MyBinaryTreeMain {
+	  public static void main(String[] args) {
+
+	        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+	        myBinaryTree.add(56);
+	        myBinaryTree.add(30);
+	        myBinaryTree.add(70);
+
+	        int size = myBinaryTree.getSize();
+	        System.out.println("Size of binary tree is: "+size);
+	        System.out.println("Binary Tree is :");
+	        myBinaryTree.print();
+	    }
+
+}
